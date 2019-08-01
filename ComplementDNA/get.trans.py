4816422acd9c51e88b6c476b24882c75fc44a5a6
Meng Luo <czheluo@gmail.com>
2019-08-01 15:33:07 +0800

    Complementing a Strand of DNA
    Complementing a Strand of DNA

--- ComplementDNA/get.trans.py
+++ ComplementDNA/get.trans.py
@@ -19,55 +19,6 @@ parser.add_argument('-i', '--fa',required=True, help=' a fasta file ')
 parser.add_argument('-o', '--out',required=True, help='ouput file name')
 args = parser.parse_args()
 
-'''
-if not args.fa:
-	strs="python get.count.py --fa file.fa --out get.count"
-	print(strs)
-	stop
-
-def countnc( s ):
-  nc = { 'A': 0 , 'C': 0 , 'G': 0 , 'T': 0 }
-  for base in s:
-    if nc.has_key(base):
-      nc[base] += 1
-  res = ''+ str(nc['A'])+ ' '+ str(nc['C'])+ ' '+ str(nc['G'])+ ' '+ str(nc['T'])
-  #print res
-  return res
-with open(args.fa) as inp,open(args.out,"w") as oup:
-	for line in inp:
-		#print(line)
-		#break
-		if re.match(">", line):
-			spl=line.split(">")
-			#print(spl[1])
-			#break
-		else:
-			cnc=countnc(line)
-			#cnc.insert(0,str(spl[1]))
-			print(cnc)
-			#resu= ''+str(spl[1])+' '+str(cnc)
-			#print(resu)
-			oup.write(cnc)
-
-alt_map = {'ins':'0'}
-complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
-
-def reverse_complement(seq):    
-    for k,v in alt_map.iteritems():
-        seq = seq.replace(k,v)
-    bases = list(seq) 
-    bases = reversed([complement.get(base,base) for base in bases])
-    bases = ''.join(bases)
-    for k,v in alt_map.iteritems():
-        bases = bases.replace(v,k)
-    return bases
-
->>> seq = "TCGGinsGCCC"
->>> print "Reverse Complement:"
->>> print(reverse_complement(seq))
-GGGCinsCCGA
-'''
-
 def Reverse(seq):
     rev = []
     x = len(seq)
